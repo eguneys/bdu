5d7afd2ecc1e690dbf098d81673aed591899ef2e
Emre Guneyler <eguneys@hotmail.com>
2021-03-27 21:01:51 +0000

    remove immutable

--- src/bd.ts
+++ src/bd.ts
@@ -71,100 +71,54 @@ export class DB2<A, B, C> {
   }
 }
 
-export class DBA<A, B, C> {
-  _make: (a: A, b: B) => C
-  all: Array<C>
-  space: Map<A, Map<B, C>>
+// export class DBU<A, B, C> {
+//   make: (a: A, b: B) => Array<C>
+//   all: Set<List<C>>
+//   space: Map<A, Map<B, List<C>>>
 
-  constructor(_make: (a: A, b: B) => C,
-              allA: Array<A>,
-              allB: Array<B>) {
-    this._make = _make;
-    this.space = new Map()
-    this.all = []
+//   constructor(make: (a: A, b: B) => Array<C>,
+//               allA: Array<A>,
+//               allB: Array<B>) {
+//     this.make = make;
+//     this.space = new Map()
+//     this.all = Set();
 
-    for (let a of allA) {
-      for (let b of allB) {
-        this.all.push(this.get(a, b))
-      }
-    }
-  }
+//     for (let a of allA) {
+//       for (let b of allB) {
+//         this.all.add(this._get(a, b))
+//       }
+//     }
+//   }
 
-  make(a: A, b: B): C {
-    let c = this._make(a, b);
-    
-    let _c = this.all.find(_ => deepeq(_, c));
+//   _make(a: A, b: B): List<C> {
+//     return fromJS(this.make(a, b));
+//   }
 
-    if (_c) {
-      return _c;
-    } else {
-      return c;
-    }
-  }
+//   get(a: A, b: B): Array<C> {
+//     return this._get(a, b).toJS();
+//   }
 
-  get(a: A, b: B): C {
-    let bc = this.space.get(a);
-    if (bc) {
+//   _get(a: A, b: B): List<C> {
+//     let bc = this.space.get(a);
+//     if (bc) {
 
-      let c = bc.get(b);
+//       let c = bc.get(b);
 
-      if (c) {
-        return c;
-      } else {
-        let _c = this.make(a, b);
-        bc.set(b, _c);
-        return _c;
-      }      
+//       if (c) {
+//         return c;
+//       } else {
+//         let _c = this._make(a, b);
+//         bc.set(b, _c);
+//         return _c;
+//       }      
 
-    } else {
-      let c = this.make(a, b);
-      let bc = new Map()
-        .set(b, c);
-      this.space.set(a, bc);
-      return c;
-    }
-  }
-}
+//     } else {
+//       let c = this._make(a, b);
+//       let bc = new Map()
+//         .set(b, c);
+//       this.space.set(a, bc);
+//       return c;
+//     }
+//   }
+// }
 
-export function deepeq(a: any, b: any) {
-  if (Array.isArray(a) && Array.isArray(b)) {
-    return arreq(a, b);
-  } else if (a instanceof Set && b instanceof Set) {
-    return seteq(a, b);
-  } else {
-    return a === b;
-  }
-}
-
-
-export function seteq(a: Set<any>, b: Set<any>): boolean {
-  if (a.size !== b.size) {
-    return false;
-  }
-  for (let item of a) {
-    let found = false;
-    for (let item2 of b) {
-      if (item === item2) {
-        found = true;
-        break;
-      }
-    }
-    if (!found) {
-      return false;
-    }
-  }
-  return true;
-}
-
-
-export function arreq(a: Array<any>, b: Array<any>): boolean {
-  if (a.length !== b.length) {
-    return false;
-  }
-  for (let i in a) {
-    if (!b.some(_ => _ === a[i])) {
-      return false;
-    }
-  }
-  return true;
-}
--- src/test.ts
+++ src/test.ts
@@ -2,14 +2,6 @@ import * as bd from './bd';
 
 export default function() {
 
-  let digits = [0, 1,2,3];
-
-  let ns = new bd.DB2<number, number, Array<number>>(
-    (a, b) => [a + b],
-    digits,
-    digits);
-
-  console.log(ns.get(0, 2) === ns.get(1, 1));
   
 
 }
